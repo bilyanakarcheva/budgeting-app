c8a093dbcd3a21b5f7ba28876c8cb474de056b9e
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2025-01-22 19:57:03 +0200

    Add data protection layer with process .env file

--- server/src/config/dbConnect.js
+++ server/src/config/dbConnect.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 const dbConnect = async () => {
     try {
         await mongoose.connect(
-            ""
+            process.env.MONGO_URL
         );
         console.log("Connected to MongoDB");
     } catch (error) {
