ad5e727de467b119aeaa07955e89f6013ccdc278
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2025-01-22 19:54:46 +0200

    Add fetch users route

--- server/src/routes/users/usersRoutes.js
+++ server/src/routes/users/usersRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { registerUser } from '../../controllers/users/usersController.js';
+import { registerUser, fetchUsers } from '../../controllers/users/usersController.js';
 
 export const userRoute = express.Router();
+
 userRoute.post('/register', registerUser);
+userRoute.get('/', fetchUsers);
 
