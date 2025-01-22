9d7c51ab8d818fb1130e4de3cad9e7dfcb1eb804
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2025-01-22 19:55:10 +0200

    Add hashing of user password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,5 +1,7 @@
 // Build the User model
 import mongoose from "mongoose";
+// Import bcrypt to hash passwords
+import bcrypt from 'bcryptjs';
 
 // Define the schema for the User model
 const userSchema = new mongoose.Schema({
@@ -27,8 +29,18 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 });
 
+// Hash the password before saving the user
+    userSchema.pre('save', async function(next) {
+    // Check if the password field is modified to avoid hashing the password again
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next(); // Proceed to save the user 
+   })
 
 // Compile the schema into a model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema); 
 
 export default User;
