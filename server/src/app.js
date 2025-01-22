c7dc65eea177d38e8140a51e2e5f857139d734d2
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2025-01-22 19:54:18 +0200

    Add error handling and users route handler to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,27 @@
 // Import all the business logic and routes here
 import express from 'express';
+import dotenv from 'dotenv';
 import dbConnect from './config/dbConnect.js';
 import { userRoute } from './routes/users/usersRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 // Connection to MongoDB database
 const app = express();
+
+// env 
+dotenv.config();
 
 // dbConnect
 dbConnect();
 
 // Middleware - a function that connects the request and response objects
-const logger = (req, res, next) => {
-    console.log("I am a logger");
-    next();
-};
-app.use(logger);
-app.use('/', userRoute);
+app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+
+app.use('/api/users', userRoute);
+
+// Error handler
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
 
